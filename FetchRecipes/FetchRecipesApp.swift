b7df63edf4c2ef634ea67edaa704c6e202a44a66
Bianca <bmcurutan@gmail.com>
2024-11-05 19:11:26 -0800

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Bianca Curutan on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
